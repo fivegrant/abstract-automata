f98f4888d99c0ac1eeb4dad87c16c745564d4a6f
Five Grant <5@fivegrant.com>
2021-07-29 08:41:40 -0500

    Make some stack fixes

--- src/App/Storage/Stack.hs
+++ src/App/Storage/Stack.hs
@@ -5,14 +5,7 @@ import App.Storage.Storage
 data Stack = Stack Input Input 
 
 instance Storage Stack where
-  focus Stack Nothing _ = Nothing
-  focus Stack top remain = (Maybe top, remain)
-  write Nothing (Stack top "") = Stack Nothing ""
-  write Nothing (Stack top (x:xs)) = Stack Maybe x xs 
-  write x (Stack top remain) = Stack x (top:remain)
-
-  focus (Input Nothing _) = [Nothing]
-  focus (Input top remain) = [top]
-  write _ x = x
-  shift (Input _ (x:[])) _ = Input Nothing ""
-  shift (Input _ (x:xs)) _ = Input (Just x) xs
+  focus Stack input pile = [focus input , focus pile]
+  write x (Stack input (Input top remain)) = Stack input (Input x remain)
+  shift (Stack input (Input Nothing remaining)) _ = Stack (shift input) (shift remaining)
+  shift (Stack input remaining) _ = Stack (shift input) remaining
